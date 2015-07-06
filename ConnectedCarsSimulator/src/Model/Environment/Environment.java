ffe4b8d612a6e448b14c42e01a3b29c15fbc25cc
Avzgui <ri_antoine@yahoo.fr>
2015-07-06 14:42:15 +0200

    Adding GNU license in the files' comments

--- ConnectedCarsSimulator/src/Model/Environment/Environment.java
+++ ConnectedCarsSimulator/src/Model/Environment/Environment.java
@@ -1,8 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2015 Antoine "Avzgui" Richard
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 package Model.Environment;
 
 import Utility.CardinalPoint;
@@ -18,121 +31,119 @@ import java.util.Map.Entry;
  * @author Antoine "Avzgui" Richard
  */
 public class Environment {
-    
+
     private Table<Integer, Integer, Infrastructure> map;
     // private ArrayList<Vehiclar_Body>
-    
+
     /**
      * Environment's constructor
      */
-    public Environment(){
+    public Environment() {
         this.map = TreeBasedTable.create();
     }
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public Table<Integer, Integer, Infrastructure> getMap() {
         return map;
     }
 
     /**
-     * 
-     * @param map 
+     *
+     * @param map
      */
     public void setMap(Table<Integer, Integer, Infrastructure> map) {
         this.map = map;
     }
-    
+
     /**
-     * 
+     *
      * @param x
      * @param y
      * @param nb_ways
      * @param ways_size
-     * @param indonesian_cross 
+     * @param indonesian_cross
      */
-    public void addIntersection(int x, int y, 
+    public void addIntersection(int x, int y,
             Table<Flow, CardinalPoint, Integer> nb_ways,
             Table<Flow, CardinalPoint, Integer> ways_size,
-            boolean indonesian_cross){
-        
-        System.out.println("Try to add intersection in position [" 
-                + x + ", " + y + "]");
-        
+            boolean indonesian_cross) {
+
         //Intersection creation, real position not defined yet
         Intersection intersection = new Intersection(0, 0, nb_ways, ways_size, indonesian_cross);
-        
+
         //If the map is empty and x = 0 and y = 0
-        if(this.map.isEmpty()){
-            if(x == 0 && y == 0)
+        if (this.map.isEmpty()) {
+            if (x == 0 && y == 0) {
                 this.map.put(x, y, intersection);
-            else
-                System.out.println("The first intersection should be in position [0, 0] not [" 
+            } else {
+                System.out.println("The first intersection should be in position [0, 0] not ["
                         + x + ", " + y + "]");
-        }
-        else{
-            
+            }
+        } else {
+
             //Future position
             Cell cell = null;
-            
+
             //To determinate the new Intersection's position, check his future neighbors
             HashMap<CardinalPoint, Infrastructure> neighbors = new HashMap<>();
-            neighbors.put(CardinalPoint.NORTH, this.map.get(x, y-1));
-            neighbors.put(CardinalPoint.EAST, this.map.get(x+1, y));
-            neighbors.put(CardinalPoint.SOUTH, this.map.get(x, y+1));
-            neighbors.put(CardinalPoint.WEST, this.map.get(x-1, y));
-            
+            neighbors.put(CardinalPoint.NORTH, this.map.get(x, y - 1));
+            neighbors.put(CardinalPoint.EAST, this.map.get(x + 1, y));
+            neighbors.put(CardinalPoint.SOUTH, this.map.get(x, y + 1));
+            neighbors.put(CardinalPoint.WEST, this.map.get(x - 1, y));
+
             //For each neighbor not null
-            for(Entry<CardinalPoint, Infrastructure> entry : neighbors.entrySet()){
-                if(entry.getValue() != null){
+            for (Entry<CardinalPoint, Infrastructure> entry : neighbors.entrySet()) {
+                if (entry.getValue() != null) {
                     //Get the the cell determinate by the neighbor
                     Cell c = entry.getValue().getCellForAnotherInfrastructure(
-                                entry.getKey().getFront(),
-                                intersection.getWidth(), 
-                                intersection.getHeight());
-                    
+                            entry.getKey().getFront(),
+                            intersection.getWidth(),
+                            intersection.getHeight());
+
                     //If cell is null, affect c
-                    if(cell == null)
+                    if (cell == null) {
                         cell = c;
-                    else if(c.getX() != cell.getX() && c.getY() != cell.getY())
+                    } else if (c.getX() != cell.getX() && c.getY() != cell.getY()) {
                         System.out.println("The intersection you want to add in"
-                                + " position [" + x + ", " + y 
+                                + " position [" + x + ", " + y
                                 + "] haven't good size");
+                    }
                 }
             }
-            
+
             //If cell is not null, update intersection's position and add it to the map
-            if(cell != null){
+            if (cell != null) {
                 intersection.setPosition(cell.getX(), cell.getY());
                 this.map.put(x, y, intersection);
-            }
-            else
+            } else {
                 System.out.println("The intersection need neighbors");
+            }
         }
     }
-    
+
     /**
-     * 
+     *
      * @return An array of all the cells in the map
      */
-    public ArrayList<Cell> getCells(){
+    public ArrayList<Cell> getCells() {
         ArrayList<Cell> cells = new ArrayList<>();
-        
+
         //For each infrastructure in the map
-        for(Infrastructure i : this.map.values()){
+        for (Infrastructure i : this.map.values()) {
             //Add cells
             cells.addAll(i.getCells());
         }
-        
+
         return cells;
     }
-    
+
     /**
-     * 
+     *
      */
-    public void removeAll(){
-       this.map.clear();
+    public void removeAll() {
+        this.map.clear();
     }
 }
