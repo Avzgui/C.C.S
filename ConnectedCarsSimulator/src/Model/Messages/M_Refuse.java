c9d9623ed04123b8fa95a72e236e82ef9e141fc4
Avzgui <ri_antoine@yahoo.fr>
2015-07-17 09:12:08 +0200

    M_Accept, M_Refuse and M_Argue classes created

---
+++ ConnectedCarsSimulator/src/Model/Messages/M_Refuse.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2015 Antoine "Avzgui" Richard and collaborators
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package Model.Messages;
+
+import Utility.Crossing_Configuration;
+
+/**
+ * The class M_Accept, inherited by M_Conf, is used when a vehicule refuse a
+ * configuration proposed.
+ * 
+ * @author Antoine "Avzgui" Richard
+ */
+public class M_Refuse extends M_Conf {
+
+    public M_Refuse(int sender_id, int receiver_id,
+            Crossing_Configuration current, int conf_id) {
+        super(sender_id, receiver_id, current);
+        this.datum.add(conf_id);
+    }
+    
+}
